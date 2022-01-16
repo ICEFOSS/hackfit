970e6335f3b7f0a1b82ee54bbf631c017bf78f61
Aman K. Shihab <amanshihab276@gmail.com>
2022-01-16 23:15:31 +0530

    Added FAQ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Landing from "./components/landing";
 import Prize from "./components/prizes";
 import Nav from "./components/nav";
+import Faq from "./components/faq";
 const App = () => {
   return (
     <div className="font-sans text-white bg-brochgreen">
       <Nav />
       <Landing />
       <Prize />
+      <Faq />
     </div>
   );
 };
