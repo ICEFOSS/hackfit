a2841300d708435e95731afcbea684c6082a8eb6
Aman K. Shihab <amanshihab276@gmail.com>
2022-01-17 00:17:18 +0530

    Added hover effects

--- src/components/faq.js
+++ src/components/faq.js
@@ -11,7 +11,7 @@ export default function Faq() {
           <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="text-xl font-semibold  bg-gray-200 rounded-t py-2 px-4">
+                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How Long is this site live?
                 </summary>
 
@@ -22,7 +22,7 @@ export default function Faq() {
                 </p>
               </details>
               <details class="mb-4">
-                <summary class="text-xl font-semibold bg-gray-200 rounded-t py-2 px-4">
+                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   Can I install/upload anything I want on there?
                 </summary>
 
@@ -33,7 +33,7 @@ export default function Faq() {
                 </p>
               </details>
               <details class="mb-4">
-                <summary class="text-xl font-semibold  bg-gray-200 rounded-t py-2 px-4">
+                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How can I migrate to another site?
                 </summary>
 
@@ -46,7 +46,7 @@ export default function Faq() {
             </div>
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="text-xl font-semibold  bg-gray-200 rounded-t py-2 px-4">
+                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   Can I change the domain you give me?
                 </summary>
 
@@ -57,7 +57,7 @@ export default function Faq() {
                 </p>
               </details>
               <details class="mb-4">
-                <summary class="text-xl font-semibold  bg-gray-200 rounded-t py-2 px-4">
+                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How many sites I can create at once?
                 </summary>
 
@@ -68,7 +68,7 @@ export default function Faq() {
                 </p>
               </details>
               <details class="mb-4">
-                <summary class="text-xl font-semibold  bg-gray-200 rounded-t py-2 px-4">
+                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How can I communicate with you?
                 </summary>
 
