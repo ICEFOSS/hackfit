4795547fc88c5cb9466a9a6198c1d565600b6056
Amar1013 <amarjithraj67@gmail.com>
2022-01-17 01:18:33 +0530

    second commit prizes updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Landing from "./components/landing";
-import Prize from "./components/Prize";
+import Prize from "./components/prizes";
+import Nav from "./components/nav";
 const App = () => {
   return (
-    <div className="font-sans">
+    <div className="font-sans text-white bg-brochgreen">
+      <Nav />
       <Landing />
       <Prize />
     </div>
