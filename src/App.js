6ccc277128a73ed2fb308e86a480d01808c3b4f9
Amar1013 <amarjithraj67@gmail.com>
2022-01-16 23:58:10 +0530

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Landing from "./components/landing";
+import Prize from "./components/Prize";
 const App = () => {
   return (
     <div className="font-sans">
       <Landing />
+      <Prize />
     </div>
   );
 };
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,8 +1,8 @@
 export default function Landing() {
   return (
     <h1 className="text-3xl font-bold underline font-red-900">
-      {" "}
-      Hello world! x{" "}
+      {/* {" "}
+      Hello world! x{" "} */}
     </h1>
   );
 }
