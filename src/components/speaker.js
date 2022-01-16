dbfd7201f05b87cc2795faeaa5182da70e76139d
Not J <Joel-Nickson>
2022-01-17 00:46:24 +0530

    speakers card component complete

--- src/components/speaker.js
+++ src/components/speaker.js
@@ -1,13 +1,60 @@
 import Speaker_card from "./speaker_card";
 
-// call speaker card with props
+const speakers = [
+    {
+        "profile_img": "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+        "name": "Saurabh",
+        "designation": "CEO",
+        "describe": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    // {
+    //     "profile_img": "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    //     "name": "Saurabh",
+    //     "designation": "CEO",
+    //     "describe": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    // },
+    // {
+    //     "profile_img": "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    //     "name": "Saurabh",
+    //     "designation": "CEO",
+    //     "describe": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    // },
+    // {
+    //     "profile_img": "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+    //     "name": "Saurabh",
+    //     "designation": "CEO",
+    //     "describe": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    // },
+]
+
 export default function Speaker() {
     return (
+
         <section>
             <div className="bg-brochgreen text-white py-8">
                 <h1 className="mx-6 mt-1 font-pixe text-center text-white text-5xl md:text-6xl sm:text-center sm:mx-0">
                     Speakers
                 </h1>
+
+                <div className="w-full bg-gray-800">
+                    <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                            {
+                                speakers.map((speaker, ind) => (
+                                    <Speaker_card
+                                        key={speaker.name + ind}
+                                        profile_img={speaker.profile_img}
+                                        name={speaker.name}
+                                        designation={speaker.designation}
+                                        describe={speaker.describe}
+                                    />
+                                ))
+                            }
+
+                        </div>
+                    </section >
+                </div >
+
             </div>
         </section>
     );
