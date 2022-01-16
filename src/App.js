37f13085b986a68071fdf4eefcd39481ed574f6d
Paul-es <paul.eliassojan@gmail.com>
2022-01-16 23:33:31 +0530

    deleted alphinejs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Landing from "./components/landing";
 import Prize from "./components/prizes";
 import Nav from "./components/nav";
+import Schedule from "./components/schedule"
 const App = () => {
   return (
     <div className="font-sans text-white bg-brochgreen">
       <Nav />
       <Landing />
       <Prize />
+      <Schedule/>
     </div>
   );
 };
