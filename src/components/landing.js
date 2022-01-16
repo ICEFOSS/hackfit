2d17cd2b4b68dd9ab0c4c3d2916005c50790669d
Naveen B Jacob <naveenbjacob123@gmail.com>
2022-01-16 17:57:19 +0530

    added the previous landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,18 +1,10 @@
 export default function Landing() {
   return (
     <div className="h-screen flex justify-center flex-col">
-      <div className="flex justify-center items-center flex-col">
+      <div className="h-screen relative bg-[url('https://swag.ee/nfg/4657e5688a55402d.svg')] bg-[length:126px_126px] animate-[90s_scroll_infinite_linear] opacity-10"></div>
+      <div className="absolute opacity-100 flex flex-col items-center">
         <div className="text-8xl">HACKFIT</div>
         <div className="text-2xl"> 28TH - 30Th JAN 2022</div>
-      </div>
-      <div className="px-8 text-5xl py-4">
-        <div>Supportors</div>
-        <div className="flex justify-evenly flex-wrap">
-          <div className="border-4 border-black h-24 w-24 bg-white"></div>
-          <div className="border-4 border-black h-24 w-24 bg-white"></div>
-          <div className="border-4 border-black h-24 w-24 bg-white"></div>
-          <div className="border-4 border-black h-24 w-24 bg-white"></div>
-        </div>
       </div>
     </div>
   );
--- src/components/prizes.js
+++ src/components/prizes.js
@@ -1,7 +1,16 @@
 const Prize = () => {
   return (
-    <div>
-      <div>Prizes</div>
+    <div className="px-8 flex justify-center flex-col">
+      <div className="text-5xl py-4">
+        <div>Supportors</div>
+        <div className="flex justify-evenly flex-wrap">
+          <div className="border-4 border-black h-24 w-24 bg-white"></div>
+          <div className="border-4 border-black h-24 w-24 bg-white"></div>
+          <div className="border-4 border-black h-24 w-24 bg-white"></div>
+          <div className="border-4 border-black h-24 w-24 bg-white"></div>
+        </div>
+      </div>
+      <div className="text-3xl">Prizes</div>
     </div>
   );
 };
