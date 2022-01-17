b75de045da2ce7133cd1f90bc624d2a15b79da6a
Amar1013 <amarjithraj67@gmail.com>
2022-01-17 13:58:56 +0530

    conflict solved

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Schedule from "./components/schedule";
 import Supported from "./components/supported";
 import Nav from './components/nav';
 import Landing from './components/landing';
-import Prize from './components/Prize'
+import Prize from './components/Prize';
 
 const App = () => {
   return (
