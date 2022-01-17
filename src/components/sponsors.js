0315a079371c8f34ecfb7f10f7bf853a78480aa5
Aman K. Shihab <amanshihab276@gmail.com>
2022-01-18 02:35:53 +0530

    WIP sponsors component

---
+++ src/components/sponsors.js
@@ -0,0 +1,34 @@
+export default function Sponsors() {
+  return (
+    <div>
+      <section class="container px-6 py-4 mx-auto">
+        <h3 class="text-center text-4xl sm:text-6xl leading-normal tracking-tight text-gray-900">
+          Our <span class="text-gray-300">Sponsors</span>
+        </h3>
+        <div class="grid gap-6 mb-8 md:grid-cols-2 lg:grid-cols-4 my-7">
+          <div class="flex items-center p-4 rounded-lg">
+            <div>
+              <img src="/sponsor-plh.png" />
+            </div>
+          </div>
+          <div class="flex items-center p-4 rounded-lg">
+            <div>
+              <img src="/sponsor-plh.png" />
+            </div>
+          </div>
+          <div class="flex items-center p-4 rounded-lg">
+            <div>
+              <img src="/sponsor-plh.png" />
+            </div>
+          </div>
+          <div class="flex items-center p-4 rounded-lg">
+            <div>
+              <img src="/sponsor-plh.png" />
+            </div>
+          </div>
+        </div>
+      </section>
+      );
+    </div>
+  );
+}
