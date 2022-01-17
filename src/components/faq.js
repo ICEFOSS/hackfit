1152ffcd92b32d30a50b1a8bb52634a8d2d3833d
Aman K. Shihab <amanshihab276@gmail.com>
2022-01-17 19:32:54 +0530

    Made the components bigger

--- src/components/faq.js
+++ src/components/faq.js
@@ -4,40 +4,49 @@ export default function Faq() {
       <section class="text-gray-700">
         <div class="container px-5 py-24 mx-auto">
           <div class="text-center mb-20">
-            <h3 class="text-center text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900">
+            <h3 class="text-center text-4xl sm:text-6xl leading-normal tracking-tight text-gray-900">
               Frequently Asked <span class="text-gray-300">Questions</span>
             </h3>
           </div>
           <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How Long is this site live?
                 </summary>
 
-                <p class="bg-slate-300 rounded-b px-2">
+                <p
+                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  style={{ fontFamily: "sans-serif" }}
+                >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
                   Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
                   minim velit nostrud pariatur culpa magna in aute.
                 </p>
               </details>
               <details class="mb-4">
-                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
-                  Can I install/upload anything I want on there?
+                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+                  Can I install anything I want on there?
                 </summary>
 
-                <p class="bg-slate-300 rounded-b px-2">
+                <p
+                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  style={{ fontFamily: "sans-serif" }}
+                >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
                   Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
                   minim velit nostrud pariatur culpa magna in aute.
                 </p>
               </details>
               <details class="mb-4">
-                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How can I migrate to another site?
                 </summary>
 
-                <p class="bg-slate-300 rounded-b px-2">
+                <p
+                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  style={{ fontFamily: "sans-serif" }}
+                >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
                   Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
                   minim velit nostrud pariatur culpa magna in aute.
@@ -46,33 +55,42 @@ export default function Faq() {
             </div>
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   Can I change the domain you give me?
                 </summary>
 
-                <p class="bg-slate-300 rounded-b px-2">
+                <p
+                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  style={{ fontFamily: "sans-serif" }}
+                >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
                   Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
                   minim velit nostrud pariatur culpa magna in aute.
                 </p>
               </details>
               <details class="mb-4">
-                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How many sites I can create at once?
                 </summary>
 
-                <p class="bg-slate-300 rounded-b px-2">
+                <p
+                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  style={{ fontFamily: "sans-serif" }}
+                >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
                   Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
                   minim velit nostrud pariatur culpa magna in aute.
                 </p>
               </details>
               <details class="mb-4">
-                <summary class="text-xl font-semibold rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How can I communicate with you?
                 </summary>
 
-                <p class="bg-slate-300 rounded-b px-2">
+                <p
+                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  style={{ fontFamily: "sans-serif" }}
+                >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
                   Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
                   minim velit nostrud pariatur culpa magna in aute.
