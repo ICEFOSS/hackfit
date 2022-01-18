151e149a01a74b729ac44b64be6f24cab660204f
Aman K. Shihab <amanshihab276@gmail.com>
2022-01-18 23:55:32 +0530

    Added sponsors

--- src/components/sponsor_card.js
+++ src/components/sponsor_card.js
@@ -1,10 +1,10 @@
 export default function Sponsor_card(props) {
   return (
     <>
-      <div className="w-full md:w-2/5 h-full">
+      <div className="w-full sm:min-w-10">
         <a href={props.url}>
           <img
-            className="object-center object-cover w-full h-full"
+            className="object-center object-cover w-full"
             src={props.logo}
             alt="photo"
           />
--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -16,6 +16,21 @@ const diamond_sponsors = [
     logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
     url: "#",
   },
+  {
+    name: "Name",
+    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+    url: "#",
+  },
+  {
+    name: "Name",
+    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+    url: "#",
+  },
+  {
+    name: "Name",
+    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+    url: "#",
+  },
 ];
 
 const gold_sponsors = [
@@ -71,14 +86,14 @@ export default function Sponsor() {
         <h3 class="text-center text-4xl sm:text-6xl leading-normal tracking-tight text-gray-900">
           Our <span class="text-gray-300">Sponsors</span>
         </h3>
-        <h2 className="mx-6 mt-1 font-pixe text-center text-gray-500 text-5xl md:text-6xl sm:text-center sm:mx-0">
+        <h2 className="mx-6 mt-1 font-pixe text-center text-gray-500 text-5xl md:text-6xl sm:text-6xl sm:text-center sm:mx-0">
           Diamond Sponsors
         </h2>
 
         <div className="w-full bg-brochgreen">
-          <section className="max-w-md mx-auto px-4 py-12">
-            <div className="grid grid-cols-4 lg:grid-cols-2 grid-rows-1 gap-4">
-              {gold_sponsors.map((sponsor, ind) => (
+          <section className="max-w-3xl mx-auto px-4 py-12">
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-5">
+              {diamond_sponsors.map((sponsor, ind) => (
                 <Sponsor_card
                   key={sponsor.name + ind}
                   url={sponsor.url}
@@ -94,8 +109,8 @@ export default function Sponsor() {
         </h2>
 
         <div className="w-full bg-brochgreen">
-          <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <section className="max-w-md mx-auto px-4 py-12">
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 grid-rows-1 gap-5">
               {gold_sponsors.map((sponsor, ind) => (
                 <Sponsor_card
                   key={sponsor.name + ind}
@@ -112,9 +127,9 @@ export default function Sponsor() {
         </h2>
 
         <div className="w-full bg-brochgreen">
-          <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-              {gold_sponsors.map((sponsor, ind) => (
+          <section className="max-w-md mx-auto px-4 py-12">
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 grid-rows-1 gap-5">
+              {platinum_sponsors.map((sponsor, ind) => (
                 <Sponsor_card
                   key={sponsor.name + ind}
                   url={sponsor.url}
