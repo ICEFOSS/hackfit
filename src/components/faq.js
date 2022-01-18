5d7f5404678e853b52a4f9d6207434176f6ef22c
Naveen B Jacob <naveenbjacob123@gmail.com>
2022-01-18 13:15:42 +0530

    nav responsive

--- src/components/faq.js
+++ src/components/faq.js
@@ -1,22 +1,22 @@
 export default function Faq() {
   return (
     <div>
-      <section class="text-gray-700">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="text-center mb-20">
-            <h3 class="text-center text-4xl sm:text-6xl leading-normal tracking-tight text-gray-900">
-              Frequently Asked <span class="text-gray-300">Questions</span>
+      <section className="text-gray-700">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="text-center mb-20">
+            <h3 className="text-center text-4xl sm:text-6xl leading-normal tracking-tight text-gray-900">
+              Frequently Asked <span className="text-gray-300">Questions</span>
             </h3>
           </div>
-          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-            <div class="w-full lg:w-1/2 px-4 py-2">
-              <details class="mb-4">
-                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+          <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div className="w-full lg:w-1/2 px-4 py-2">
+              <details className="mb-4">
+                <summary className="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How Long is this site live?
                 </summary>
 
                 <p
-                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  className="text-2xl bg-slate-300 rounded-b p-4 px-6"
                   style={{ fontFamily: "sans-serif" }}
                 >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
@@ -24,13 +24,13 @@ export default function Faq() {
                   minim velit nostrud pariatur culpa magna in aute.
                 </p>
               </details>
-              <details class="mb-4">
-                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+              <details className="mb-4">
+                <summary className="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   Can I install anything I want on there?
                 </summary>
 
                 <p
-                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  className="text-2xl bg-slate-300 rounded-b p-4 px-6"
                   style={{ fontFamily: "sans-serif" }}
                 >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
@@ -38,13 +38,13 @@ export default function Faq() {
                   minim velit nostrud pariatur culpa magna in aute.
                 </p>
               </details>
-              <details class="mb-4">
-                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+              <details className="mb-4">
+                <summary className="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How can I migrate to another site?
                 </summary>
 
                 <p
-                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  className="text-2xl bg-slate-300 rounded-b p-4 px-6"
                   style={{ fontFamily: "sans-serif" }}
                 >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
@@ -53,14 +53,14 @@ export default function Faq() {
                 </p>
               </details>
             </div>
-            <div class="w-full lg:w-1/2 px-4 py-2">
-              <details class="mb-4">
-                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+            <div className="w-full lg:w-1/2 px-4 py-2">
+              <details className="mb-4">
+                <summary className="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   Can I change the domain you give me?
                 </summary>
 
                 <p
-                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  className="text-2xl bg-slate-300 rounded-b p-4 px-6"
                   style={{ fontFamily: "sans-serif" }}
                 >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
@@ -68,13 +68,13 @@ export default function Faq() {
                   minim velit nostrud pariatur culpa magna in aute.
                 </p>
               </details>
-              <details class="mb-4">
-                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+              <details className="mb-4">
+                <summary className="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How many sites I can create at once?
                 </summary>
 
                 <p
-                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  className="text-2xl bg-slate-300 rounded-b p-4 px-6"
                   style={{ fontFamily: "sans-serif" }}
                 >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
@@ -82,13 +82,13 @@ export default function Faq() {
                   minim velit nostrud pariatur culpa magna in aute.
                 </p>
               </details>
-              <details class="mb-4">
-                <summary class="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
+              <details className="mb-4">
+                <summary className="text-3xl rounded-t transition duration-200 hover:scale-105 bg-gray-200 py-2 px-4">
                   How can I communicate with you?
                 </summary>
 
                 <p
-                  class="text-2xl bg-slate-300 rounded-b p-4 px-6"
+                  className="text-2xl bg-slate-300 rounded-b p-4 px-6"
                   style={{ fontFamily: "sans-serif" }}
                 >
                   Laboris qui labore cillum culpa in sunt quis sint veniam.
