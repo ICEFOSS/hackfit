d8e95ac342102445cd75b00df2e36a3d68658371
Aman K. Shihab <amanshihab276@gmail.com>
2022-01-19 01:11:56 +0530

    Updated the colours

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -49,19 +49,9 @@ const gold_sponsors = [
     logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
     url: "#",
   },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
 ];
 
 const platinum_sponsors = [
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
   {
     name: "Name",
     logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
@@ -83,62 +73,65 @@ export default function Sponsor() {
   return (
     <section>
       <div className="bg-brochgreen text-white py-8">
-        <h3 class="text-center text-4xl sm:text-6xl leading-normal tracking-tight text-gray-900">
+        <h3 class="text-center text-4xl sm:text-6xl leading-normal tracking-tight text-gray-900 mb-16">
           Our <span class="text-gray-300">Sponsors</span>
         </h3>
-        <h2 className="mx-6 mt-1 font-pixe text-center text-gray-500 text-5xl md:text-6xl sm:text-6xl sm:text-center sm:mx-0">
-          Diamond Sponsors
-        </h2>
-
-        <div className="w-full bg-brochgreen">
-          <section className="max-w-3xl mx-auto px-4 py-12">
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-5">
-              {diamond_sponsors.map((sponsor, ind) => (
-                <Sponsor_card
-                  key={sponsor.name + ind}
-                  url={sponsor.url}
-                  logo={sponsor.logo}
-                  name={sponsor.name}
-                />
-              ))}
-            </div>
-          </section>
+        <div class="p-10">
+          <h2 className="mx-6 mt-1 font-pixe text-center text-diamond text-5xl md:text-6xl sm:text-6xl sm:text-center sm:mx-0">
+            Diamond Sponsors
+          </h2>
+          <div className="w-full bg-brochgreen">
+            <section className="max-w-3xl mx-auto px-4 py-12">
+              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-28">
+                {diamond_sponsors.map((sponsor, ind) => (
+                  <Sponsor_card
+                    key={sponsor.name + ind}
+                    url={sponsor.url}
+                    logo={sponsor.logo}
+                    name={sponsor.name}
+                  />
+                ))}
+              </div>
+            </section>
+          </div>
         </div>
-        <h2 className="mx-6 mt-1 font-pixe text-center text-yellow-400 text-5xl md:text-6xl sm:text-center sm:mx-0">
-          Gold Sponsors
-        </h2>
-
-        <div className="w-full bg-brochgreen">
-          <section className="max-w-md mx-auto px-4 py-12">
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 grid-rows-1 gap-5">
-              {gold_sponsors.map((sponsor, ind) => (
-                <Sponsor_card
-                  key={sponsor.name + ind}
-                  url={sponsor.url}
-                  logo={sponsor.logo}
-                  name={sponsor.name}
-                />
-              ))}
-            </div>
-          </section>
+        <div class="p-10">
+          <h2 className="mx-6 mt-1 font-pixe text-center text-gold text-5xl md:text-6xl sm:text-6xl sm:text-center sm:mx-0">
+            Gold Sponsors
+          </h2>
+          <div className="w-full bg-brochgreen">
+            <section className="max-w-3xl mx-auto px-4 py-12">
+              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-28">
+                {gold_sponsors.map((sponsor, ind) => (
+                  <Sponsor_card
+                    key={sponsor.name + ind}
+                    url={sponsor.url}
+                    logo={sponsor.logo}
+                    name={sponsor.name}
+                  />
+                ))}
+              </div>
+            </section>
+          </div>
         </div>
-        <h2 className="mx-6 mt-1 font-pixe text-center text-gray-400 text-5xl md:text-6xl sm:text-center sm:mx-0">
-          Platinum Sponsors
-        </h2>
-
-        <div className="w-full bg-brochgreen">
-          <section className="max-w-md mx-auto px-4 py-12">
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 grid-rows-1 gap-5">
-              {platinum_sponsors.map((sponsor, ind) => (
-                <Sponsor_card
-                  key={sponsor.name + ind}
-                  url={sponsor.url}
-                  logo={sponsor.logo}
-                  name={sponsor.name}
-                />
-              ))}
-            </div>
-          </section>
+        <div class="p-10">
+          <h2 className="mx-6 mt-1 font-pixe text-center text-platinum text-5xl md:text-6xl sm:text-6xl sm:text-center sm:mx-0">
+            Platinum Sponsors
+          </h2>
+          <div className="w-full bg-brochgreen">
+            <section className="max-w-3xl mx-auto px-4 py-12">
+              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-28">
+                {platinum_sponsors.map((sponsor, ind) => (
+                  <Sponsor_card
+                    key={sponsor.name + ind}
+                    url={sponsor.url}
+                    logo={sponsor.logo}
+                    name={sponsor.name}
+                  />
+                ))}
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     </section>
