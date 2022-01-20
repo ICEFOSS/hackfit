734cc3303694f67cfb73acdb7a3a52dc50715ab3
Joel-Nickson <Joel-Nickson>
2022-01-21 01:42:38 +0530

    modularized code and fixed responsiveness

--- src/components/sponsor_card.js
+++ src/components/sponsor_card.js
@@ -1,10 +1,10 @@
 export default function Sponsor_card(props) {
   return (
     <>
-      <div className="w-full sm:min-w-10">
+      <div className="w-8/12 sm:min-w-10 place-self-center">
         <a href={props.url}>
           <img
-            className="object-center object-cover w-full"
+            className="object-center object-cover w-3/4 mx-8"
             src={props.logo}
             alt="photo"
           />
@@ -13,3 +13,107 @@ export default function Sponsor_card(props) {
     </>
   );
 }
+const all_sponsors_names = ["Diamond", "Gold", "Platinum"];
+const all_sponsors_Txt_color = ["text-diamond", "text-gold", "text-platinum"];
+
+export function Sponsor_each_section(props) {
+  const all_sponsors_names_style = ` font-pixe text-center ${all_sponsors_Txt_color[props.sponsor_index]} text-5xl md:text-6xl sm:text-center sm:mx-0 mx-auto px-4`;
+
+  return (
+    <>
+      <div className="p-4  w-full ">
+        <h2 className={all_sponsors_names_style}>
+          {all_sponsors_names[props.sponsor_index]} Sponsors
+        </h2>
+        <div className="w-full bg-brochgreen">
+          <section className="max-w-xs md:max-w-xl lg:max-w-3xl mx-auto px-4 py-12">
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-x-1 gap-y-4 sm:gap-y-16">
+              {all_sponsors[props.sponsor_index].map((sponsor, ind) => (
+                <Sponsor_card
+                  key={sponsor.name + ind}
+                  url={sponsor.url}
+                  logo={sponsor.logo}
+                  name={sponsor.name}
+                />
+              ))}
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const all_sponsors = [
+  // diamond_sponsors 
+  [
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+  ],
+
+  // gold_sponsors = 
+  [
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+  ],
+
+  // platinum_sponsors = 
+  [
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+    {
+      name: "Name",
+      logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
+      url: "#",
+    },
+  ]
+];
--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,157 +1,17 @@
-import Sponsor_card from "./sponsor_card";
-
-const diamond_sponsors = [
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-];
-
-const gold_sponsors = [
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-];
-
-const platinum_sponsors = [
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-  {
-    name: "Name",
-    logo: "https://plchldr.co/i/130x50?&bg=000000&fc=ffffff&text=Placeholder",
-    url: "#",
-  },
-];
+import Sponsor_card, { Sponsor_each_section } from "./sponsor_card";
 
 export default function Sponsor() {
   return (
-    <section>
-      <div className="bg-brochgreen text-white py-8">
-        <h3 class="text-center text-4xl sm:text-6xl leading-normal tracking-tight text-gray-900 mb-16">
-          Our <span class="text-gray-300">Sponsors</span>
+    <section className="ml-0 sm:ml-14 md:ml-28 lg:ml-10">
+      <div className="bg-brochgreen text-white py-8 overflow-x-clip">
+        <h3 className="text-center text-5xl leading-normal tracking-tight text-gray-900 mb-16">
+          Our <span className="text-gray-300">Sponsors</span>
         </h3>
-        <div class="p-10">
-          <h2 className="mx-6 mt-1 font-pixe text-center text-diamond text-5xl md:text-6xl sm:text-6xl sm:text-center sm:mx-0">
-            Diamond Sponsors
-          </h2>
-          <div className="w-full bg-brochgreen">
-            <section className="max-w-3xl mx-auto px-4 py-12">
-              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-28">
-                {diamond_sponsors.map((sponsor, ind) => (
-                  <Sponsor_card
-                    key={sponsor.name + ind}
-                    url={sponsor.url}
-                    logo={sponsor.logo}
-                    name={sponsor.name}
-                  />
-                ))}
-              </div>
-            </section>
-          </div>
-        </div>
-        <div class="p-10">
-          <h2 className="mx-6 mt-1 font-pixe text-center text-gold text-5xl md:text-6xl sm:text-6xl sm:text-center sm:mx-0">
-            Gold Sponsors
-          </h2>
-          <div className="w-full bg-brochgreen">
-            <section className="max-w-3xl mx-auto px-4 py-12">
-              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-28">
-                {gold_sponsors.map((sponsor, ind) => (
-                  <Sponsor_card
-                    key={sponsor.name + ind}
-                    url={sponsor.url}
-                    logo={sponsor.logo}
-                    name={sponsor.name}
-                  />
-                ))}
-              </div>
-            </section>
-          </div>
-        </div>
-        <div class="p-10">
-          <h2 className="mx-6 mt-1 font-pixe text-center text-platinum text-5xl md:text-6xl sm:text-6xl sm:text-center sm:mx-0">
-            Platinum Sponsors
-          </h2>
-          <div className="w-full bg-brochgreen">
-            <section className="max-w-3xl mx-auto px-4 py-12">
-              <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-28">
-                {platinum_sponsors.map((sponsor, ind) => (
-                  <Sponsor_card
-                    key={sponsor.name + ind}
-                    url={sponsor.url}
-                    logo={sponsor.logo}
-                    name={sponsor.name}
-                  />
-                ))}
-              </div>
-            </section>
-          </div>
-        </div>
+
+        <Sponsor_each_section sponsor_index='0' />
+        <Sponsor_each_section sponsor_index='1' />
+        <Sponsor_each_section sponsor_index='2' />
       </div>
     </section>
   );
 }
-
-// function Sponsor_card(props) {
-//   return (
-//     <>
-//       <div className="w-full bg-brochwhite rounded-lg sahdow-lg overflow-hidden flex flex-col md:flex-row ">
-//         <div className="w-full md:w-2/5 h-80">
-//           <a href={props.url}>
-//             <img
-//               className="object-center object-cover w-full h-full"
-//               src={props.logo}
-//               alt="photo"
-//             />
-//           </a>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
