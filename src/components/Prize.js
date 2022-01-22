f35b474189a64151e8619faf66844a2f63187178
JoelNickson <joel2000nickson@gmail.com>
2022-01-22 19:08:29 +0530

    changed grid to flex and reduced size on prize

--- src/components/Prize.js
+++ src/components/Prize.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 export default class Prize extends Component {
   render() {
     return (
-      <section className=" ml-0 md:ml-36  px-12 sm:px-6 lg:px-4 py-12 mx-auto">
+      <section className=" ml-0 md:ml-36  px-3 sm:px-6 lg:px-4 py-12 mx-auto">
         <div className="text-center pb-12">
           <h1 className="text-3xl md:text-4xl lg:text-7xl font-heading text-white">
             prizes
           </h1>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-9 justify-items-center mx-auto">
+        <div className="flex flex-wrap flex-row gap-3 justify-center mx-auto">
 
           <Prize_card
             cash="10000"
@@ -40,7 +40,7 @@ export default class Prize extends Component {
 function Prize_card(props) {
   return (
     <>
-      <div className="transform max-w-sm sm:max-w-1/4  bg-brochblack rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row transition duration-500 w-full hover:scale-105">
+      <div className="transform max-w-sm bg-brochblack shadow-lg overflow-hidden flex flex-col md:flex-row transition duration-200 w-60 hover:scale-105">
         <div className="w-full h-52 sm:h-64 md:h-72 lg:h-80">
           <img
             className="p-3 w-24 sm:w-32 lg:w-40 mx-auto"
@@ -51,7 +51,7 @@ function Prize_card(props) {
             {props.cash} <span className="text-green-600">$</span>
           </p>
           <p className={`text-center ${props.text_color} font-bold text-xl sm:text-2xl`}>
-            1st prize
+            {props.prize_name}
           </p>
         </div>
       </div>
