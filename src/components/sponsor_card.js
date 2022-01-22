0e94083bd9813aeac1d8788d07352841058324c6
JoelNickson <joel2000nickson@gmail.com>
2022-01-22 19:33:50 +0530

    changed grid to flex and reduced spacing between sponsors

--- src/components/sponsor_card.js
+++ src/components/sponsor_card.js
@@ -1,10 +1,10 @@
 export default function Sponsor_card(props) {
   return (
-    <section className="col-span-12 md:col-span-6 lg:col-span-4 md:mx-auto p-2">
+    <section className="col-span-12 md:col-span-6 lg:col-span-4 md:mx-auto my-3">
       <div className="w-full">
         <a href={props.url}>
           <img
-            className="object-center object-cover w-full mx-8"
+            className="object-center object-cover w-full "
             src={props.logo}
             alt="photo"
           />
@@ -17,16 +17,16 @@ const all_sponsors_names = ["Diamond", "Gold", "Platinum"];
 const all_sponsors_Txt_color = ["text-diamond", "text-gold", "text-platinum"];
 
 export function Sponsor_each_section(props) {
-  const all_sponsors_names_style = ` font-pixe text-center ${all_sponsors_Txt_color[props.sponsor_index]} text-5xl md:text-6xl mx-4`;
+  const all_sponsors_names_style = `text-center ${all_sponsors_Txt_color[props.sponsor_index]} text-5xl md:text-6xl mx-4`;
 
   return (
     <>
-      <div className="p-4  w-full ">
+      <div className=" w-full ">
         <h2 className={all_sponsors_names_style}>
           {all_sponsors_names[props.sponsor_index]} Sponsors
         </h2>
         <div className="w-full bg-brochgreen">
-          <section className="max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-3xl mx-auto px-4 py-12">
+          <section className="max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-3xl mx-auto px-4 py-6 pb-10">
             <div className="flex flex-row flex-wrap gap-y-6 sm:gap-y-10 md:gap-y-16 w-full">
               <div className="flex flex-wrap flex-row mx-8  flex-grow justify-around">
                 {all_sponsors[props.sponsor_index].map((sponsor, ind) => (
