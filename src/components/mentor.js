605ce56cc588262313ff26144d460c2ae5c394e5
JoelNickson <joel2000nickson@gmail.com>
2022-01-27 00:28:04 +0530

    added margins on mentors

--- src/components/mentor.js
+++ src/components/mentor.js
@@ -7,20 +7,22 @@ export default class Mentor extends Component {
 
   render() {
     return (
-      <section className="ml-0 md:ml-36"
+      <section className="ml-0 md:ml-36 mt-28"
         id='Mentors'>
         <div className='bg-brochgreen py-8'>
           <div className="py-4">
             {/*Acm directives contact*/}
-            <h3 className="text-center text-5xl leading-normal  item-center tracking-tight mb-6 flex justify-center">
+            <h3 className="text-center text-5xl leading-normal  item-center tracking-tight mb-6 md:mb-0 flex justify-center">
               Mentors
             </h3>
             <div className="w-full bg-brochgreen">
-              <section className="max-w-xl sm:max-w-2xl lg:max-w-4xl mx-auto px-4 py-6 pb-12">
-                <div className="flex flex-row flex-wrap gap-y-0 justify-evenly">
+              <section className="max-w-xl sm:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto px-4 py-6 pb-12">
+                <div className="flex flex-row flex-wrap justify-evenly">
 
                   {Present.map((item) => (
-                    <Mentor_each_section item={item} />
+                    <Mentor_each_section
+                      item={item}
+                    />
                   ))}
                 </div>
               </section>
@@ -39,7 +41,7 @@ function Mentor_each_section(props) {
         <div
           className="sm:pt-2 md:pt-0 mx-4 text-center justify-between aspect-auto main-test "
           style={{
-            backgroundImage: `url(https://plchldr.co/i/500x250)`,
+            backgroundImage: `url(${props.item.image})`,
           }}
         >
           <div>
@@ -48,13 +50,13 @@ function Mentor_each_section(props) {
           </div>
           <div className="pt-0 md:pt-9 font-light" >
             <div className="flex justify-center main-test-opposite">
-              {/* <a href={props.item.linkedin} target="__blank"> */}
-              <Linkedin_svg />
-              {/* </a>
-              <a href={props.item.linkedin} target="__blank"> */}
-              <p className='px-1' />
-              <Github_svg />
-              {/* </a> */}
+              <a href={props.item.linkedin} target="__blank">
+                <Linkedin_svg />
+              </a>
+              <a href={props.item.linkedin} target="__blank">
+                <p className='px-1' />
+                <Github_svg />
+              </a>
               {/*<p className="mx-2"></p>*/}
             </div>
           </div>
