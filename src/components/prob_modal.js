831a54c71732d9989884b9715c650be15f0bf417
JoelNickson <joel2000nickson@gmail.com>
2022-01-29 03:54:11 +0530

    renamed prob_stmt_model to prob_modal.js

---
+++ src/components/prob_modal.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import "../styles/Modal.css";
+
+Modal.setAppElement("#root");
+
+export default function App() {
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    return (
+        <div className="App text-white mt-8">
+            <button onClick={() => setIsOpen(true)}>Open Modal</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={() => setIsOpen(false)}
+                overlayClassName={{
+                    base: "overlay-base",
+                    afterOpen: "overlay-after",
+                    beforeClose: "overlay-before"
+                }}
+                className={{
+                    base: "content-base",
+                    afterOpen: "content-after",
+                    beforeClose: "content-before"
+                }}
+                closeTimeoutMS={500}
+            >
+
+                <button onClick={() => setIsOpen(false)}>Close Modal</button>
+            </Modal>
+        </div>
+    );
+}
+
+// export default function Modal() {
+//     const [showModal, setShowModal] = React.useState(false);
+//     return (
+//         <>
+//             <button
+//                 className=" text-white uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 mt-8 font-bold bg-opacity-0"
+//                 type="button"
+//                 onClick={() => setShowModal(true)}
+//             >
+//                 Read More
+//             </button>
+//             {showModal ? (
+//                 <>
+//                     <div
+//                         className="justify-center items-center flex w-96 overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+//                     >
+//                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
+//                             {/*content*/}
+//                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+//                                 {/*header*/}
+//                                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+//                                     <h3 className="text-3xl font-semibold">
+//                                         Modal Title
+//                                     </h3>
+//                                     <button
+//                                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+//                                         onClick={() => setShowModal(false)}
+//                                     >
+//                                         <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+//                                             ×
+//                                         </span>
+//                                     </button>
+//                                 </div>
+//                                 {/*body*/}
+//                                 <div className="relative p-6 flex-auto">
+//                                     <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+//                                         I always felt like I could do anything. That’s the main
+//                                         thing people are controlled by! Thoughts- their perception
+//                                         of themselves! They're slowed down by their perception of
+//                                         themselves. If you're taught you can’t do anything, you
+//                                         won’t do anything. I was taught I could do everything.
+//                                     </p>
+//                                 </div>
+//                                 {/*footer*/}
+//                                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+//                                     <button
+//                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+//                                         type="button"
+//                                         onClick={() => setShowModal(false)}
+//                                     >
+//                                         Close
+//                                     </button>
+//                                     <button
+//                                         className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+//                                         type="button"
+//                                         onClick={() => setShowModal(false)}
+//                                     >
+//                                         Save Changes
+//                                     </button>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+//                 </>
+//             ) : null}
+//         </>
+//     );
+// }
--- src/components/prob_statement.js
+++ src/components/prob_statement.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { stayfit_with_social_distancing, mental_health_issues, report_drug_trafficking } from './constants'
-import Modal from "./prob_stmt_model";
+import Modal from "./prob_modal";
 
 export default function ProbState() {
     return (
@@ -77,6 +77,7 @@ function Supporters_each_section(props) {
                             </p>
 
                             <Modal />
+                            {/* <Modal openButtonText="hi" children={undefined} /> */}
                         </div>
                     </div>
                 </a>
