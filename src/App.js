105b2ea389a3f1af7ce68b7cd828f63993b9d201
JoelNickson <joel2000nickson@gmail.com>
2022-01-30 16:48:08 +0530

    added lazy loading to improved performance & SEO's

--- src/App.js
+++ src/App.js
@@ -1,65 +1,127 @@
 import Landing from "./components/landing";
-import Prizes from "./components/Prize";
 import Nav from "./components/nav";
-import Speaker from "./components/speaker";
-import Faq from "./components/faq";
-import Supporter from "./components/supporter";
-import Mentor from './components/mentor'
-import Schedule from "./components/schedule";
-import Sponsors from "./components/sponsors";
-import Footer from "./components/footer";
-import About from "./components/about";
 import ProbState from "./components/prob_statement";
+import About from "./components/about";
+import Supporter from "./components/supporter";
+import Prizes from "./components/Prize";
+import Speaker from "./components/speaker";
+import Schedule from "./components/schedule";
+import Mentor from './components/mentor'
+import Sponsors from "./components/sponsors";
+import Faq from "./components/faq";
+import Footer from "./components/footer";
+import LazyLoad from 'react-lazyload';
 import './styles/App.css';
 
 const App = () => {
   return (
-    <div className="font-sans text-white bg-green-600">
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Nav />
-      </section>
+    <div className="text-white bg-green-600">
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Nav />
+        </section>
+      </LazyLoad>
+
       <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
         <Landing />
       </section>
-      <div className="spacer layer1" />
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <ProbState />
-      </section>
-      <div className="spacer layer1" />
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <About />
-      </section>
-      <div className="spacer layer1" />
-      <section className=' from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Supporter />
-      </section>
-      <div className="spacer layer1" />
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Prizes />
-      </section>
-      <div className="spacer layer1" />
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Speaker />
-      </section>
-      <div className="spacer layer1" />
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Schedule />
-      </section>
-      <div className="spacer layer1" />
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Mentor />
-      </section>
-      <div className="spacer layer1" />
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Sponsors />
-      </section>
-      <div className="spacer layer1" />
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Faq />
-      </section>
-      <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
-        <Footer />
-      </section>
+
+      <LazyLoad height={300} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <ProbState />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <About />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className=' from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Supporter />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={200} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Prizes />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={200} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Speaker />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={400} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Schedule />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={200} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Mentor />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={400} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Sponsors />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={200} offset={300} once>
+        <div className="spacer layer1" />
+      </LazyLoad>
+
+      <LazyLoad height={300} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Faq />
+        </section>
+      </LazyLoad>
+
+      <LazyLoad height={200} offset={300} once>
+        <section className='from-brochgreen to-green-600 bg-gradient-to-t'>
+          <Footer />
+        </section>
+      </LazyLoad>
     </div>
   );
 };
