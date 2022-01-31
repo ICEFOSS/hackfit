3aba78760e101a58be9a990448fb613adbb740a2
JoelNickson <joel2000nickson@gmail.com>
2022-01-31 12:06:27 +0530

    added form to reg now link in landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,15 +4,15 @@ export default function Landing() {
       <div className="bg-landingImage bg-[length:126px_126px] animate-[wiggle_90s_scroll_infinite_linear] h-screen flex justify-items-center items-center md:justify-center lg:justify-start">
         <div className="px-5 sm:pl-10 lg:pl-44">
           <h1 className="text-6xl font-semibold font-sans mb-6">
-            <span className="text-red-500">HACKFIT</span> <br />
+            <span className="text-red-600 shadow-sm stroke">HACKFIT</span> <br />
             <span>20TH - 30TH JAN 2022</span>
           </h1>
-          <p className="text-lg max-w-md">
+          <p className="text-lg max-w-md shadow-sm">
             HACKFIT is a 36-hour Hackathon conducted as part of ICEFOSS 2022 commencing from the 3rd & 4th of March 2022.
           </p>
-          <button className="inline-block mt-10 px-10 py-3 bg-red-500 text-lg text-white">
+          <a href="https://forms.gle/ug6pPtr4SbHxArEK8" target="_blank" className="inline-block mt-10 px-10 py-3 bg-red-600 text-lg text-white rounded-md shadow-2xl">
             REGISTER NOW
-          </button>
+          </a>
         </div>
       </div>
     </section>
