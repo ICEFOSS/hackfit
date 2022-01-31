2e7e65de9974233d7065ee503a8f33eb7a4b85e8
JoelNickson <joel2000nickson@gmail.com>
2022-01-31 14:58:18 +0530

    Register link now points to Problem Statement

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,19 +4,19 @@ export default function Landing() {
     <section className="ml-0 md:ml-36">
       <div className="context">
         <div className="px-5 sm:pl-10 lg:pl-44 flex flex-col w-full">
-          <h1 className="text-6xl font-semibold font-sans mb-6 text-center z-50">
+          <h1 className="text-7xl font-semibold font-sans mb-6 text-center z-50">
 
-            <span className="text-rose-600 shadow-sm justify-start flex">HACKFIT</span>
-            <span className="text-white text-xs font-mono justify-start flex">An ICEFOSS initiative</span> <br />
-            <span className="flex justify-start ">20TH - 30TH JAN 2022</span>
+            <span className="text-rose-600 shadow-sm justify-start flex text-7xl">HACKFIT</span>
+            <span className="text-white text-xs font-mono justify-start flex">An ICEFOSS initiative</span>
+            <span className="flex justify-start text-5xl mt-8">3RD - 5TH MAR 2022</span>
           </h1>
           <p className="text-lg shadow-sm justify-start max-w-lg z-50 flex">
             HACKFIT is a 36-hour Hackathon conducted as part of ICEFOSS 2022 commencing from the 3rd & 4th of March 2022.
           </p>
           <div className="w-full justify-start flex">
-            <a href="https://forms.gle/YonV7YehKwU8Y3Hn9" target="_blank" className="mt-10 px-10 py-3 bg-red-600 text-lg text-white w-60 rounded-md shadow-2xl z-50">
+            <button onClick={() => (document.getElementById("PStatement").scrollIntoView())} className="mt-10 px-10 py-3 bg-red-600 text-lg text-white w-44 text-center rounded-md shadow-2xl z-50">
               REGISTER NOW
-            </a>
+            </button>
           </div>
 
         </div>
@@ -51,6 +51,6 @@ export default function Landing() {
           </a>
         </div>
       </div> */}
-    </section>
+    </section >
   );
 }
